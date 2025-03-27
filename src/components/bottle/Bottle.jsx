f35e0e1e62ceb484363933aec961a55d3b8b7cec
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-28 04:49:14 +0600

    added to btn added

--- src/components/bottle/Bottle.jsx
+++ src/components/bottle/Bottle.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import './bottle.css'
+import "./bottle.css";
 
-const Bottle = ({ bottle }) => {
-  console.log(bottle);
-  const {
-    img,
-    name,
-    price,
-    ratings,
-    ratingsCount,
-    seller,
-    shipping,
-    stock,
-  } = bottle;
+const Bottle = ({ bottle, handleAddToCart }) => {
+
+  const { img, name, price, ratings, ratingsCount, seller, shipping, stock } =
+    bottle;
   return (
     <div className="card bottle">
       <img src={img} alt="bottle image" />
       <h3>{name}</h3>
-     <div className="title-div"> <div><h6>Price: {price}</h6>
-      <h6>{stock} Remaining</h6></div>
-      <div><h6>Ratings: {ratings}</h6>
-      <h6>RatingCount: {ratingsCount}</h6></div>
-      <div><h6>Seller: {seller}</h6>
-      <h6>Shipping: {shipping}</h6></div>
+      <div className="title-div">
+        {" "}
+        <div>
+          <h6>Price: ${price}</h6>
+          <h6>{stock} Remaining</h6>
+        </div>
+        <div>
+          <h6>Ratings: {ratings} <i class="fa-regular fa-star"></i></h6>
+          <h6>RatingCount: {ratingsCount}</h6>
+        </div>
+        <div>
+          <h6>Seller: {seller}</h6>
+          <h6>Shipping: {shipping}</h6>
+        </div>
       </div>
-      <button>Buy Now</button>
+      <button onClick={()=>handleAddToCart(bottle)}>Buy Now</button>
     </div>
   );
 };
--- src/components/bottles/Bottles.jsx
+++ src/components/bottles/Bottles.jsx
@@ -1,19 +1,31 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Bottle from "../bottle/bottle";
-import './bottles.css'
+import "./bottles.css";
 
 const Bottles = ({ bottlesPromise }) => {
+  const [cart, setCart] = useState([]);
+
   const bottles = use(bottlesPromise);
+  const handleAddToCart = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
 
-  return <div>
-
-    <h3>Bottles: {bottles.length}</h3>
-    <div className="bottles-container">
-    {
-      bottles.map(bottle=> <Bottle key={bottle.id} bottle={bottle}></Bottle>)
-    }
+  return (
+    <div>
+      <h3>Bottles: {bottles.length}</h3>
+      <p>Added to Cart: {cart.length}</p>
+      <div className="bottles-container">
+        {bottles.map((bottle) => (
+          <Bottle
+            handleAddToCart={handleAddToCart}
+            key={bottle.id}
+            bottle={bottle}
+          ></Bottle>
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Bottles;
