ddd1888ea940c9c5aeb876783926fcdad994014b
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-27 23:34:16 +0600

    project setup and data fetch done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+import "./App.css";
+import Bottles from "./components/Bottles";
+
+const bottlesPromise = fetch("/bottles/bottles.json").then((res) =>res.json());
+
+
+function App() {
+  return (
+    <>
+      <h1>Buy Awesome Water Bottle</h1>
+      <Suspense fallback={<h3>Bottles ara loading....</h3>}>
+        <Bottles bottlesPromise={bottlesPromise}></Bottles>
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
