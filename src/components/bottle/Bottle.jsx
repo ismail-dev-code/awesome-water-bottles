fe073d9824ecf608028127904a7ef786c99a820b
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-28 03:27:31 +0600

    btn added

--- src/components/bottle/Bottle.jsx
+++ src/components/bottle/Bottle.jsx
@@ -18,12 +18,13 @@ const Bottle = ({ bottle }) => {
       <img src={img} alt="bottle image" />
       <h3>{name}</h3>
      <div className="title-div"> <div><h6>Price: {price}</h6>
-      <h6>Stock: {stock}</h6></div>
+      <h6>{stock} Remaining</h6></div>
       <div><h6>Ratings: {ratings}</h6>
       <h6>RatingCount: {ratingsCount}</h6></div>
       <div><h6>Seller: {seller}</h6>
       <h6>Shipping: {shipping}</h6></div>
       </div>
+      <button>Buy Now</button>
     </div>
   );
 };
